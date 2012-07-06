4a62bcc2741518329f38636895bbe1ee6f3d46d1
alanktwong <alan_kt_wong@yahoo.com>
2012-07-05 22:06:14 -0400

    Start Chap 14

--- src/main/scala/scala/impatient/Exercises14.scala
+++ src/main/scala/scala/impatient/Exercises14.scala
@@ -1,8 +1,94 @@
 package scala.impatient
 
+import java.math.MathContext
 /**
  * Pattern Matching and Case Classes
  */
-class Exercises14 {
-
+object Exercises14 {
+  /**
+   * (14.2)
+   */
+  def swap(pair: Tuple2[Int,Int]): Tuple2[Int,Int] = {
+    val result = pair match {
+      case(x,y) => (y,x)
+    }
+    result
+  }
+  
+  /*
+   * (14.3)
+   */
+  def swap[T](lst: List[T]): List[T] = {
+    lst match {
+      case x::y::tail => (y::x::tail)
+      case _ => lst
+    }
+  }
+  /**
+   * (14.4)
+   */
+  abstract class Item {
+    def price: Double
+    def description: String
+  }
+  case class Product(val price: Double, val description: String) extends Item
+  case class Bundle(val description: String) extends Item {
+    var items = List[Item]()
+    def price : Double = {
+      // this has obvious rounding errors
+      val prices = items.map(_.price)
+      var total = prices.reduceLeft(_ + _)
+      total
+    }
+    def add(item:Item) = {
+      items = item :: items
+    }
+  }
+  case class Mutiple(val quantity: Int, val product: Item, val description: String) extends Item {
+    def price : Double = {
+      // this has obvious rounding errors
+      val total = product.price * quantity;
+      total
+    }
+  }
+  
+  /**
+   * (14.5)
+   */
+  private def leafSumImpl(total:Int, nodes:List[Any]): Int = {
+    nodes match {
+      case List() => total
+      case head::tail => 
+        head match {
+          case x:Int => leafSumImpl(x + total, tail)
+          case y:List[Any] => leafSumImpl(total, y ::: tail)
+          case _ => total
+        }
+    }
+  }
+  def leafSum(tree:List[Any]): Int = {
+    val result = leafSumImpl(0, tree)
+    result
+  }
+  /**
+   * (14.6)
+   */
+  sealed abstract class BTrie
+  case class Leaf(value:Int) extends BTrie
+  case class BinaryNode(left:BTrie, right:BTrie) extends BTrie
+  case class Node(nodes:BTrie*) extends BTrie
+  
+  def treeSumImpl(total:Int, tree: BTrie):Int = {
+    tree match {
+      case leaf : Leaf => total + leaf.value
+      case binaryNode: BinaryNode => treeSumImpl(total, binaryNode.left) + treeSumImpl(total, binaryNode.right)
+      case node: Node =>
+        node.nodes.foldLeft(total) { (runningTotal,eachNode) =>
+          runningTotal + treeSumImpl(0,eachNode)
+        }
+    }
+  }
+  def treeSum(tree:BTrie): Int = {
+    treeSumImpl(0, tree)
+  }
 }
---
+++ src/test/scala/scala/impatient/Exercises14Suite.scala
@@ -0,0 +1,66 @@
+package scala.impatient
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import org.scalatest.junit.JUnitRunner
+
+import scala.collection.mutable.Stack
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class Exercises14Suite extends FunSuite with BeforeAndAfter {
+  before {
+    println("executing before")
+  }
+
+  test("14.2: swap integers") {
+    expect( (2,1) ) { Exercises14.swap(1,2) }
+  }
+
+  test("14.3: swap first two elements of list") {
+    var arry = List("foo","bar","barry","baz")
+    expect( List("bar","foo","barry","baz") ) { Exercises14.swap(arry) }
+    arry = List("foo","bar");
+    expect( List("bar","foo") ) { Exercises14.swap(arry) }
+    arry = List("foo");
+    expect( List("foo") ) { Exercises14.swap(arry) }
+    arry = List[String]();
+    expect( true ) { Exercises14.swap(arry).isEmpty }
+  }
+  
+  test("14.4: case class for Items") {
+    val product0 = scala.impatient.Exercises14.Product(3.14, "Blackwell Toaster")
+    val product1 = scala.impatient.Exercises14.Product(13.14, "Ginzu Knife")
+    val product2 = scala.impatient.Exercises14.Product(272.52, "Kitchen Aid Stand Mixer")
+    expect( 3.14 ) { product0.price }
+    
+    val multiple = scala.impatient.Exercises14.Mutiple(10, product0, "10 Toasters")
+    expect( product0.price * 10 ) { multiple.price }
+    
+    val wishList = scala.impatient.Exercises14.Bundle("Wedding List")
+    wishList.add(product0)
+    wishList.add(product1)
+    wishList.add(product2)
+    expect( product0.price + product1.price + product2.price ) { wishList.price }
+    
+    wishList.add(multiple)
+    expect( product0.price + product1.price + product2.price + multiple.price ) { wishList.price }
+    
+  }
+  
+  test("14.5: recursive descent of tree") {
+    expect( 0 ) { Exercises14.leafSum( List() ) }
+    expect( 2 ) { Exercises14.leafSum( List(1,1) ) }
+    expect( 7 ) { Exercises14.leafSum( List(2, List(5) ) ) }
+    expect( 18 ) { Exercises14.leafSum( List(List(3,8), 2, List(5) ) ) }
+  }
+  
+  test("14.6: recursive descent of tree") {
+    import scala.impatient.Exercises14.{BTrie, Node, Leaf, BinaryNode}
+    expect( 1 ) { Exercises14.treeSum( Leaf(1) ) }
+    expect( 2 ) { Exercises14.treeSum( Node(Leaf(1),Leaf(1)) ) }
+    expect( 7 ) { Exercises14.treeSum( Node(Leaf(2), Node(Leaf(5),Leaf(0)) ) ) }
+    expect( 18 ) { Exercises14.treeSum( BinaryNode(BinaryNode(Leaf(3),Leaf(8)), BinaryNode(Leaf(2),Leaf(5)) ) ) }
+    expect( 18 ) { Exercises14.treeSum( Node(Node(Leaf(3),Leaf(8)), Leaf(2),Node(Leaf(5)) ) ) }
+  }
+}
