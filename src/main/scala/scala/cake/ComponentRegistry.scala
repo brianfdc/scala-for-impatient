f2dc8cb993a60e10bd1f4be9f0cc93748bfded5e
alanktwong <alan_kt_wong@yahoo.com>
2012-07-09 17:56:19 -0400

    Add documentation for real world scala

--- src/main/scala/scala/cake/ComponentRegistry.scala
+++ src/main/scala/scala/cake/ComponentRegistry.scala
@@ -1,8 +1,16 @@
 package scala.cake
 
 /**
+ * For Real World Scala series, see
+ * <ul>
+ * <li>http://jonasboner.com/2008/10/01/real-world-scala-introduction/</li>
+ * <li>http://jonasboner.com/2008/10/06/real-world-scala-dependency-injection-di/</li>
+ * <li>http://jonasboner.com/2008/12/09/real-world-scala-managing-cross-cutting-concerns-using-mixin-composition-and-aop/</li>
+ * <li>http://jonasboner.com/2008/12/11/real-world-scala-fault-tolerant-concurrent-asynchronous-components/</li>
+ * <li>http://jonasboner.com/2008/02/06/aop-style-mixin-composition-stacks-in-scala/</li>
+ * </ul>
  * 
- * @see http://jonasboner.com/2008/10/06/real-world-scala-dependency-injection-di/
+ * 
  * @see http://vimeo.com/23547652
  */
 object ComponentRegistry extends
