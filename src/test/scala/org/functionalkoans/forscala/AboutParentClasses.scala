53f3a99b04542be95fbd41efdd50cfd7a0f697f3
alanktwong <alan_kt_wong@yahoo.com>
2012-07-08 21:36:40 -0400

    Work on parent classes koan

--- src/test/scala/org/functionalkoans/forscala/AboutParentClasses.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutParentClasses.scala
@@ -1,45 +1,48 @@
 package org.functionalkoans.forscala
 
-import org.functionalkoans.forscala.support.KoanSuite
+import org.functionalkoans.forscala.support.KoanSpec
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
-class AboutParentClasses extends KoanSuite {
-  koan("Class heirarchy is linear, a class can only extend from one parent class") {
-    class Worker(firstName: String, lastName: String) {}
-    class Employee(firstName: String, lastName: String, employeeID: Long) extends Worker(firstName, lastName)
+class AboutParentClasses
+  extends KoanSpec("Meditate on AboutParentClasses to understand inheritance")
+{
+  "Class hierarchy" must {
+    "Be linear, a class can only extend from one parent class" in {
+      val worker = new Worker("Alan", "Wong")
+      val employee = new Employee("Alan", "Wong", 123)
+      (worker.firstName) should be (employee.firstName)
+    }
   }
-
-  koan("A class that extends from another is polymorphic") {
-    class Worker(val firstName: String, val lastName: String) {}
-    class Employee(override val firstName: String, override val lastName: String,
-                   val employeeID: Long) extends Worker(firstName, lastName)
-
-    val me = new Employee("Name", "Yourself", 1233)
-    val worker: Worker = me
-
-    worker.firstName should be(__)
-    worker.lastName should be(__)
+  
+  "A class that extends from another" must {
+    "be polymorphic" in {
+      val me = new ImmutableEmployee("Name", "Yourself", 1233)
+      val worker: ImmutableWorker = me
+      worker.firstName should be("Name")
+      worker.lastName should be("Yourself")
+    }
+    
   }
-
-  koan("An abstract class, as in Java, cannot be instantiated and only inherited") {
-    abstract class Worker(val firstName: String, val lastName: String) {}
-
-    // if you uncomment this line, if will fail compilation
-    //val worker = new Worker
-  }
-
-
-  koan("An class can be placed inside an abstract class just like in java") {
-    abstract class Worker(val firstName: String, val lastName: String) {
-
+  
+  "An abstract class (as in Java)" must {
+    abstract class AbstractWorker(val firstName: String, val lastName: String) {
       class Assignment(val hours: Long) {
         // nothing to do here.  Just observe that it compiles
       }
-
+    }
+    class CityEmployee(override val firstName: String,
+                        override val lastName: String,
+                        val employeeID: Long) extends AbstractWorker(firstName, lastName)
+    "Never be instantiated and only inherited" in {
+      // if you uncomment this line, if will fail compilation
+      //val worker = new AbstractWorker
+      
+    }
+    "Be able to have inner classes just like in Java" in {
+      val employee = new CityEmployee("Mike","Bloomberg", 123)
+      val hisAssignment = new employee.Assignment(10)
     }
   }
-
-
 }
