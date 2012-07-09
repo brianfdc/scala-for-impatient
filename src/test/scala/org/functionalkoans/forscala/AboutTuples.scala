6f12cbb7a164d74e26ab3f54420dbc3d57b2e8d3
alanktwong <alan_kt_wong@yahoo.com>
2012-07-09 17:56:39 -0400

    turn into spec

--- src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
@@ -1,31 +1,34 @@
 package org.functionalkoans.forscala
 
-import org.functionalkoans.forscala.support.KoanSuite
+import org.functionalkoans.forscala.support.KoanSpec
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
-class AboutTuples extends KoanSuite {
+class AboutTuples extends KoanSpec("Meditate on AboutTuples to get tuples") {
+  "Tuples" can {
+    "Be created easily" in {
+      val tuple = ("apple", "dog")
+      tuple._1 should be("apple")
+      tuple._2 should be("dog")
+    }
+    "Be accessed individually" in {
+      val tuple = ("apple", "dog")
+      val fruit = tuple._1
+      val animal = tuple._2
 
-  koan("Tuples can be created easily") {
-    val tuple = ("apple", "dog")
-    tuple should be(__)
+      fruit should be("apple")
+      animal should be("dog")
+      
+    }
+    "Be of mixed type" in {
+      import java.util.Date
+      val tuple5 = ("a", 1, 2.2, new Date(), BigDecimal(5))
+      tuple5._1 should be("a")
+      tuple5._2 should be(1)
+      tuple5._5 should be(BigDecimal(5))
+    }
   }
 
-  koan("Tuple items may be accessed individually") {
-    val tuple = ("apple", "dog")
-    val fruit = tuple._1
-    val animal = tuple._2
 
-    fruit should be(__)
-    animal should be(__)
-  }
-
-  koan("Tuples may be of mixed type") {
-    import java.util.Date
-    val tuple5 = ("a", 1, 2.2, new Date(), BigDecimal(5))
-
-    tuple5._2 should be(__)
-    tuple5._5 should be(__)
-  }
 }
