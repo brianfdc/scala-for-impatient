fb817f522073a1cafd2e342f885d916a47bb44ab
alanktwong <alan_kt_wong@yahoo.com>
2012-07-24 18:29:03 -0400

    do range koan

--- src/test/scala/org/functionalkoans/forscala/AboutRange.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutRange.scala
@@ -7,29 +7,31 @@ import org.junit.runner.RunWith
 @RunWith(classOf[JUnitRunner])
 class AboutRange extends KoanSuite {
 
-  koan("Range are not inclusive at end of range") {
-    val someNumbers = Range(0, 10)
+  koan("'Until' ranges are not inclusive at end of range") {
+    val someNumbers = 0 until 10
 
-    someNumbers.size should be(__)
+    someNumbers.size should be(10)
+    someNumbers.exists( _ == 10) should be(false)
+    someNumbers.exists( _ == 9) should be(true)
+    someNumbers.exists( _ == 5) should be(true)
+    someNumbers.exists( _ == 0) should be(true)
   }
 
-  koan("Range can specify increment") {
-    val someNumbers = Range(2, 10, 3)
-
-    someNumbers.size should be(__)
+  koan("'Until' ranges can specify increment") {
+    val someNumbers = 2 until 10 by 3
+    someNumbers.size should be(3)
   }
 
-  koan("Range can indicate inclusion") {
-    val someNumbers = Range(0, 34, 2)
-    someNumbers.contains(33) should be(__)
-    someNumbers.contains(32) should be(__)
-    someNumbers.contains(34) should be(__)
+  koan("'To' ranges indicate inclusion and increments") {
+    val someNumbers = 0 to 34 by 2
+    someNumbers.contains(33) should be(false)
+    someNumbers.contains(32) should be(true)
+    someNumbers.contains(34) should be(true)
   }
 
   koan("Range can specify to include value") {
-    val someNumbers = Range(0, 34).inclusive
-
-    someNumbers.contains(34) should be(__)
+    val someNumbers = 0 to 34
+    someNumbers.contains(34) should be(true)
   }
 
 }
