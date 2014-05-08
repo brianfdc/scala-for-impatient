674c69d0280d3afd5939918268ee69fa03c92cd1
awong <alan_kt_wong@yahoo.com>
2014-05-08 13:04:44 -0400

    Move to new package

--- akka/src/main/scala/akka/scattergather/AdaptiveScatterGather.scala
+++ akka/src/main/scala/akka/scattergather/AdaptiveScatterGather.scala
@@ -1,5 +1,34 @@
 package akka.scattergather
+import collection.immutable.HashMap
+//import akka.actor.{Supervisor,ReceiveTimeout, ActorRef, Actor}
+//import akka.config.Supervision._
+//import akka.dispatch.Dispatchers
 
+import scala.collection.immutable.HashMap
+/**
+ * Scatter-gather example from end of Josh Suereth, _Scala in Depth_, chp 9 (Manning: 2012)
+ * 
+ * It relies on Akka 1 ... refactor so that it uses Akka 2
+ * 
+ * @see https://github.com/jsuereth/scala-in-depth-source
+ */
+
+/*
+object AdaptiveSearchTreeMain {
+  def submitInitialDocuments(searchNode: ActorRef) = {
+    Seq("Some example data for you",
+        "Some more example data for you to use",
+        "To be or not to be, that is the question",
+        "OMG it's a cat",
+        "This is an example.  It's a great one",
+        "HAI there", 
+        "HAI IZ HUNGRY",
+        "Hello, World",
+        "Hello, and welcome to the search node 8",
+        "The lazy brown fox jumped over the",
+        "Winning is the best because it's winning."
+    ) foreach (doc =>  searchNode ! SearchableDocument(doc))
+  } 
   def makeTree = {
     val supervisor = Supervisor(SupervisorConfig(AllForOneStrategy(List(classOf[Exception]), 3, 1000), Nil))
     val searchnodedispatcher = Dispatchers.newExecutorBasedEventDrivenDispatcher("adaptive search tree")
